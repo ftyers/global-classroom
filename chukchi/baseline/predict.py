cda2911238e4fe23ba0bff147a04f2df4319cacb
Francis Tyers <ftyers@prompsit.com>
2021-01-28 21:13:25 +0000

    add line by line comments

--- chukchi/baseline/predict.py
+++ chukchi/baseline/predict.py
@@ -1,44 +1,70 @@
 import sys, pickle
 
+# This is the filename of the model file
 model_file = sys.argv[1]
 mf = open(model_file, 'rb')
 
+# Load the unigram and bigram probabilities from the model file
 (unigrams, bigrams) = pickle.load(mf)
 
+# Initialise the probability of the start of the sentence.
 unigrams['#'] = 0.0
 
+# Hits is number of times we get a prediction right
 hits = 0
+# The number of tokens
 n_tokens = 0
+# For each of the lines in the input
 for line in sys.stdin.readlines():
+	# Split into two columns
 	row = line.strip().split('\t')
+	# Our tokens are in column one, split by space
 	tokens = row[0].split(' ')
+	# The test tokens are the beginning of sentence symbol + the list of tokens
 	tst_tokens = ['#'] + tokens
+	# Increment the number of tokens by the length of the list containing the tokens
 	n_tokens += len(tokens)
 	
+	# This is our output
 	output = []
+	# For each of the tokens in the "tst_tokens" list (e.g. the list + the beginning of sentence symbol)
 	for i in range(len(tst_tokens)-1):
-		up = 0.0
-		bp = 0.0
-		first = tst_tokens[i]
-		second = tst_tokens[i+1]
+		up = 0.0 # Unigram probability
+		bp = 0.0 # Bigram probability
+		first = tst_tokens[i] # First token in bigram
+		second = tst_tokens[i+1] # Second token in bigram
+		# If we find the first token in the bigrams dict
 		if first in bigrams:
+			# We get the best prediction for what token should come next
 			pred = max(bigrams[first], key=bigrams[first].get) 
+			# If it matches with what the next token really is
 			if pred == second:
+				# We add this whole token to the output
+				# e.g. a single click on a prediction
 				output.append(pred)
+				# Increment the number of hits by 1
 				hits += 1
-#				print('!', first, '→', pred,'|||', bigrams[first][pred], file=sys.stderr)
 			else:
+				# Otherwise we add each individual character to the output
+				# e.g. writing out each of the individual clicks
 				output += [c for c in second]
+		# If we haven't found a bigram, we just try proposing the most frequent unigram
 		else:
+			# Find the highest scoring unigram
 			pred = max(unigrams, key=unigrams.get) 
+			# If the prediction is right
 			if pred == second:
+				# Append to output
 				output.append(pred)
 				hits += 1
 #				print('!', first, '→', pred,'|||', unigrams[pred], file=sys.stderr)
 			else:
+				# Otherwise append each individual character
 				output += [c for c in second]
+		# Finally append a space symbol
 		output.append('_')
 
+	# Print out our input and the predicted sequence of keypresses
 	print('%s\t%s' % (row[0], ' '.join(output)))
 
 print('Hits:', hits, '; Tokens:', n_tokens, file=sys.stderr)
--- chukchi/baseline/train.py
+++ chukchi/baseline/train.py
@@ -1,55 +1,90 @@
 import sys, pickle
 
+# This is the training data
 training_file = sys.argv[1]
+# This is the filename that we will save the model in
 model_file = sys.argv[2]
 
+# This is a list of sentences in the training data
 corpus = []
 
+# Unigram counts = how often we see tokens, e.g. a frequency list
 unigram_counts = {}
+# Probability of the individual unigram
 unigrams = {}
-bigram_counts = {}
-bigrams = {}
+# How often we see word2 after word1 (e.g. 'house' after 'big')
+bigram_counts = {} # bigram_counts['big']['house'] = 1, bigram_counts['big']['dog'] = 1
+# Probability of word2 after word1
+bigrams = {} # bigrams['big']['house'] 0.5, ...
 
+# Read in the examples from the training data
 for line in open(training_file).readlines():
+	# Each line in the training data is two columns
+	# тыкгаткэта ивнин риӄукэтэ	ты>кгат>кэ>та ив>ни>н риӄукэ>тэ
 	row = line.strip().split('\t')
+	# The tokens are in the first column
 	tokens = row[0].split(' ')
+	# Add the tokens to the list of sentences, with a beginning of sentence and an end of sentence marker
 	corpus.append(['#'] + tokens + ['#'])		
 
 # Collect unigram counts
-n_tokens = 0
+n_tokens = 0 # Number of tokens
+# For each of the sentences in our corpus
 for sent in corpus:
+	# For each of the tokens in the sentence
 	for token in sent:
+		# If we haven't seen the token before:
 		if token not in unigram_counts:
+			# Initialise the token count to zero
 			unigram_counts[token] = 0
+		# Increment the count of that token
 		unigram_counts[token] += 1
+	# Increment the count of all the tokens
 	n_tokens += 1
 
 # Estimate unigram probabilities
+# For each of the types we have seen 
 for token in unigram_counts:
+	# The probability is the frequency of the token divided by the total number of tokens
 	unigrams[token] = unigram_counts[token]/n_tokens
 
 # Collect bigram counts
+# e.g. ['the', 'big', 'house'] 
+# bigrams: (the, big) (big, house)
+# For each of the sentences in the corpus
 for sent in corpus:
+	# For each number in the range of 0..length of sentence - 1
 	for i in range(0, len(sent)-1):
+		# The first word is the word at i
 		w1 = sent[i]
+		# The second word is the word at i+1
 		w2 = sent[i+1]
+		# If we haven't seen the first word before, initialise a new dictionary for the second word
 		if w1 not in bigram_counts:
 			bigram_counts[w1] = {}
+		# If we haven't seen the second word before, initialise the count to 0
 		if w2 not in bigram_counts[w1]:
 			bigram_counts[w1][w2] = 0
+		# Increment the count
 		bigram_counts[w1][w2] += 1
 
 n_bigrams = 0
 
 # Estimate bigram probabilities
+# For each of the first words
 for token1 in bigram_counts:
+	# If we haven't seen it before, initialise it
 	if token1 not in bigrams:
 		bigrams[token1] = {}
+	# Find the total frequency of all of the words that can go after this word
 	token_total = sum(bigram_counts[token1].values())
+	# For each of the second parts of the bigram
 	for token2 in bigram_counts[token1]:
+		# If we haven't seen the  second part before, initialise it to 0
 		if token2 not in bigrams[token1]:
 			bigrams[token1][token2] = 0		
 			n_bigrams += 1
+		# Calculate the probability
 		bigrams[token1][token2] = bigram_counts[token1][token2]/token_total
 
 # Write out model
